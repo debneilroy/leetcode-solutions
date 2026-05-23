89ded3a7a66c636c998e8bdcca8eb76e80c48421
Debneil Roy <debneilsaharoy@gmail.com>
2026-05-23 21:15:21 +0530

    Add 0078-Subsets

---
+++ Medium/0078-Subsets/solution.py
@@ -0,0 +1,15 @@
+"""
+LeetCode 78. Subsets
+Difficulty: Medium
+URL: https://leetcode.com/problems/subsets/
+"""
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        output = [[]]
+        
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        
+        return output
